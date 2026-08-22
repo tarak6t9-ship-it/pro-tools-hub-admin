f9be74a566575163a702aae9d4c6826cf34895f1
tarak6t9-ship-it <tarak6t9@gmail.com>
2026-08-22 12:52:39 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBJru5OrYxQQXfAM0r4ej7n6fVKEtjR-kA",
+  authDomain: "pro-tools-hub1.firebaseapp.com",
+  projectId: "pro-tools-hub1",
+  storageBucket: "pro-tools-hub1.firebasestorage.app",
+  messagingSenderId: "521802449640",
+  appId: "1:521802449640:web:84f69bcfd02cacaad97f5d"
+};
